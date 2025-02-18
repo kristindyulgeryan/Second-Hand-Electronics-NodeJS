0c0a9e31c14e571fa7c9f1eb30be41e6c7195f4e
Kristin <kristindyulgeryan@abv.bg>
2025-02-18 21:48:56 +0200

    Modify search functionality

--- src/controllers/electronicController.js
+++ src/controllers/electronicController.js
@@ -4,95 +4,91 @@ import { getErrorMessage } from "../utils/errorUtils.js";
 import { isAuth } from "../middlewares/authMiddleware.js";
 import { get } from "mongoose";
 
+const electronicController = Router();
 
-const electronicController=Router();
+electronicController.get("/search", async (req, res) => {
+  const filter = {
+    searchName: req.query.searchName || "",
+    searchType: req.query.searchType || "",
+  };
+  const electronics = await electronicService.getAll(filter);
 
-electronicController.get('/search', async (req,res)=>{
-    const filter=req.query;
-    const electronics = await electronicService.getAll(filter);
-
-    res.render('search', {electronics, filter})
-})
-
-electronicController.get('/catalog', async (req,res)=>{
-    const electronics =await electronicService.getAll();
-    res.render('electronics/catalog', {electronics})
-})
-
-electronicController.get('/create',(req,res)=>{
-    res.render('electronics/create')
+  res.render("search", { electronics, filter });
 });
 
-electronicController.post('/create',isAuth, async(req,res)=>{
-    const newElectronic = req.body;
-    const userId = req.user._id;
-console.log(newElectronic)
-    try {
-        await electronicService.create(newElectronic, userId);
-    } catch (err) {
-        const error = getErrorMessage(err);
-        return res.render('electronics/catalog', {
-            electronic: newElectronic,
-            error});
-        
-    }
-   
-    res.redirect('/electronics/catalog')
+electronicController.get("/catalog", async (req, res) => {
+  const electronics = await electronicService.getAll();
+  res.render("electronics/catalog", { electronics });
 });
 
-electronicController.get('/:electronicId/details', async (req,res)=>{
-    const electronicId = req.params.electronicId;
-const electronic = await electronicService.getOne(electronicId);
-
-
-const isOwner = req.user && electronic.owner.equals(req.user._id.toString());
-const isBought =  electronic.buyingList.includes(req.user?._id)
-    res.render('electronics/details', {electronic, isOwner, isBought})
+electronicController.get("/create", (req, res) => {
+  res.render("electronics/create");
 });
 
-electronicController.get('/:electronicId/buy',isAuth, async (req,res)=>{
-    const electronciId = req.params.electronicId;
-    const userId = req.user._id;
+electronicController.post("/create", isAuth, async (req, res) => {
+  const newElectronic = req.body;
+  const userId = req.user._id;
+  console.log(newElectronic);
+  try {
+    await electronicService.create(newElectronic, userId);
+  } catch (err) {
+    const error = getErrorMessage(err);
+    return res.render("electronics/catalog", {
+      electronic: newElectronic,
+      error,
+    });
+  }
 
-    try {
-        await electronicService.buy(electronciId, userId);
-       
-    } catch (err) {
-       
-     res.setError(getErrorMessage(err)); 
-    }
-    
-    res.redirect(`/electronics/${electronciId}/details`)
- })
-
-electronicController.get('/:electronicId/edit',isAuth, async (req,res)=>{
-    const electronicId = req.params.electronicId;
-    const electronic = await electronicService.getOne(electronicId);
-    res.render('electronics/edit', {electronic})
+  res.redirect("/electronics/catalog");
 });
 
+electronicController.get("/:electronicId/details", async (req, res) => {
+  const electronicId = req.params.electronicId;
+  const electronic = await electronicService.getOne(electronicId);
 
-
-electronicController.post('/:electronicId/edit',isAuth, async (req,res)=>{
-    const electronicId = req.params.electronicId;
-    const updatedElectronic = req.body;
-    try {
-        await electronicService.update(electronicId, updatedElectronic);
-    } catch (err) {
-        const error = getErrorMessage(err);
-        return res.render('electronics/edit', {
-            electronic: updatedElectronic,
-            error});
-    }
-    res.redirect(`/electronics/${electronicId}/details`)
+  const isOwner = req.user && electronic.owner.equals(req.user._id.toString());
+  const isBought = electronic.buyingList.includes(req.user?._id);
+  res.render("electronics/details", { electronic, isOwner, isBought });
 });
 
-electronicController.get('/:electronicId/delete',isAuth, async (req,res)=>{
-    const electronicId = req.params.electronicId;
-    await electronicService.deleteOne(electronicId);
-    res.redirect('/electronics/catalog')
-})
+electronicController.get("/:electronicId/buy", isAuth, async (req, res) => {
+  const electronciId = req.params.electronicId;
+  const userId = req.user._id;
 
+  try {
+    await electronicService.buy(electronciId, userId);
+  } catch (err) {
+    res.setError(getErrorMessage(err));
+  }
 
+  res.redirect(`/electronics/${electronciId}/details`);
+});
+
+electronicController.get("/:electronicId/edit", isAuth, async (req, res) => {
+  const electronicId = req.params.electronicId;
+  const electronic = await electronicService.getOne(electronicId);
+  res.render("electronics/edit", { electronic });
+});
+
+electronicController.post("/:electronicId/edit", isAuth, async (req, res) => {
+  const electronicId = req.params.electronicId;
+  const updatedElectronic = req.body;
+  try {
+    await electronicService.update(electronicId, updatedElectronic);
+  } catch (err) {
+    const error = getErrorMessage(err);
+    return res.render("electronics/edit", {
+      electronic: updatedElectronic,
+      error,
+    });
+  }
+  res.redirect(`/electronics/${electronicId}/details`);
+});
+
+electronicController.get("/:electronicId/delete", isAuth, async (req, res) => {
+  const electronicId = req.params.electronicId;
+  await electronicService.deleteOne(electronicId);
+  res.redirect("/electronics/catalog");
+});
 
 export default electronicController;
--- src/services/electronicService.js
+++ src/services/electronicService.js
@@ -1,52 +1,48 @@
-
 import Electronic from "../models/Electronic.js";
 
-
 export default {
-    async getAll(filter = {}){
+  async getAll(filter = {}) {
+    let query = Electronic.find({});
 
-        let query = Electronic.find({});
-
-        if(filter.search){
-            query = query.where('name').regex(new RegExp(filter.search, 'i'));
-        };
-
-        if(filter.searchType){
-            query = query.where({type: filter.searchType});
-        }
-
-        return query
-    },
-async getOne(electronicId){
-    return await Electronic.findById(electronicId).lean();
-},
-
-
-   async create(electronicData, userId){
-       return await  Electronic.create({...electronicData, owner: userId});
-      
-    },
-    async update(electronicId, electronicData){
-        return await Electronic.findByIdAndUpdate(electronicId, electronicData, {runValidators: true});
-    },
-    async deleteOne(electronicId){
-        return await Electronic.findByIdAndDelete(electronicId);
-    },
-
-    async buy(electronicId, userId){
-        const electronic = await Electronic.findById(electronicId);
-
-        if(electronic.owner.equals(userId)){
-            throw new Error('You cannot buy your own product!')
-        }
-
-        if(electronic.buyingList.includes(userId)){
-            throw new Error('You have already bought this product!')
-        }
-
-        electronic.buyingList.push(userId);
-
-        return electronic.save();
+    if (filter.searchName) {
+      query = query.where("name").regex(new RegExp(filter.searchName, "i"));
     }
-   
-}
+
+    if (filter.searchType) {
+      query = query.where({ type: filter.searchType });
+    }
+
+    return query;
+  },
+  async getOne(electronicId) {
+    return await Electronic.findById(electronicId).lean();
+  },
+
+  async create(electronicData, userId) {
+    return await Electronic.create({ ...electronicData, owner: userId });
+  },
+  async update(electronicId, electronicData) {
+    return await Electronic.findByIdAndUpdate(electronicId, electronicData, {
+      runValidators: true,
+    });
+  },
+  async deleteOne(electronicId) {
+    return await Electronic.findByIdAndDelete(electronicId);
+  },
+
+  async buy(electronicId, userId) {
+    const electronic = await Electronic.findById(electronicId);
+
+    if (electronic.owner.equals(userId)) {
+      throw new Error("You cannot buy your own product!");
+    }
+
+    if (electronic.buyingList.includes(userId)) {
+      throw new Error("You have already bought this product!");
+    }
+
+    electronic.buyingList.push(userId);
+
+    return electronic.save();
+  },
+};
